71d3c43cbf9f7289af8ec731c64422d01691c0e7
EmmmmDeee <158596486+EmmmmDeee@users.noreply.github.com>
2024-05-15 01:56:32 +1000

    improve

--- src/styles/dimensions.ts
+++ src/styles/dimensions.ts
@@ -1,30 +1,26 @@
 export type InputSize = 'small' | 'medium' | 'large';
 
-export const applySize = (inputSize?: InputSize) => {
-  const sizeSpecificStyles = {
-    small: `
-      font-size: 1rem;
-      border-radius: 0.25rem;
-      padding: 0.5rem 1rem;
-      margin: 0.5rem;
-    `,
-    medium: `
-      font-size: 1.5rem;
-      border-radius: 0.25rem;
-      padding: 0.75rem 1.5rem;
-      margin: 0.5rem;
-    `,
-    large: `
-      font-size: 2rem;
-      border-radius: 0.25rem;
-      padding: 1rem 1.75rem;
-      margin: 0.5rem;
-    `,
-  };
-  switch (inputSize) {
-    case 'small': return sizeSpecificStyles.small;
-    case 'medium': return sizeSpecificStyles.medium;
-    case 'large': return sizeSpecificStyles.large;
-    default: return sizeSpecificStyles.small;
-  }
+const sizeSpecificStyles: Record<InputSize, React.CSSProperties> = {
+  small: {
+    fontSize: '1rem',
+    borderRadius: '0.25rem',
+    padding: '0.5rem 1rem',
+    margin: '0.5rem',
+  },
+  medium: {
+    fontSize: '1.5rem',
+    borderRadius: '0.25rem',
+    padding: '0.75rem 1.5rem',
+    margin: '0.5rem',
+  },
+  large: {
+    fontSize: '2rem',
+    borderRadius: '0.25rem',
+    padding: '1rem 1.75rem',
+    margin: '0.5rem',
+  },
+};
+
+export const applySize = (inputSize?: InputSize): React.CSSProperties => {
+  return inputSize ? sizeSpecificStyles[inputSize] : sizeSpecificStyles.small;
 };
