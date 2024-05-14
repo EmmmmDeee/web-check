ed5d615fd31fc01807b638f27ba53bc17af64aa8
EmmmmDeee <158596486+EmmmmDeee@users.noreply.github.com>
2024-05-15 01:56:41 +1000

    improve

--- src/components/Results/DomainLookup.tsx
+++ src/components/Results/DomainLookup.tsx
@@ -1,32 +1,47 @@
-
+import React from 'react';
 import colors from 'styles/colors';
-import { Card } from 'components/Form/Card';
-import Row from 'components/Form/Row';
+import { Card, Row } from 'components/Form';
 
-const cardStyles = `
-span.val {
-  &.up { color: ${colors.success}; }
-  &.down { color: ${colors.danger}; }
-}
-`;
+type DomainDataable = {
+  data: {
+    internicData: {
+      Domain_Name?: string;
+      Creation_Date?: string;
+      Updated_Date?: string;
+      Registry_Expiry_Date?: string;
+      Registry_Domain_ID?: string;
+      Registrar_WHOIS_Server?: string;
+      Registrar?: string;
+      Registrar_URL?: string;
+      Registrar_IANA_ID?: string;
+    } | null;
+  };
+  title: string;
+  actionButtons?: JSX.Element | null;
+};
 
-const DomainLookupCard = (props: { data: any, title: string, actionButtons: any }): JSX.Element => {
+const DomainLookupCard: React.FC<DomainDataable> = (props) => {
   const domain = props.data.internicData || {};
   return (
     <Card heading={props.title} actionButtons={props.actionButtons} styles={cardStyles}>
-      { domain.Domain_Name && <Row lbl="Registered Domain" val={domain.Domain_Name} /> }
-      { domain.Creation_Date && <Row lbl="Creation Date" val={domain.Creation_Date} /> }
-      { domain.Updated_Date && <Row lbl="Updated Date" val={domain.Updated_Date} /> }
-      { domain.Registry_Expiry_Date && <Row lbl="Registry Expiry Date" val={domain.Registry_Expiry_Date} /> }
-      { domain.Registry_Domain_ID && <Row lbl="Registry Domain ID" val={domain.Registry_Domain_ID} /> }
-      { domain.Registrar_WHOIS_Server && <Row lbl="Registrar WHOIS Server" val={domain.Registrar_WHOIS_Server} /> }
-      { domain.Registrar && <Row lbl="" val="">
-        <span className="lbl">Registrar</span>
-        <span className="val"><a href={domain.Registrar_URL || '#'}>{domain.Registrar}</a></span>
-      </Row> }
-      { domain.Registrar_IANA_ID && <Row lbl="Registrar IANA ID" val={domain.Registrar_IANA_ID} /> }
+      {domain.Domain_Name && <Row key="RegisteredDomain" lbl="Registered Domain" val={domain.Domain_Name} />}
+      {domain.Creation_Date && <Row key="CreationDate" lbl="Creation Date" val={domain.Creation_Date} />}
+      {domain.Updated_Date && <Row key="UpdatedDate" lbl="Updated Date" val={domain.Updated_Date} />}
+      {domain.Registry_Expiry_Date && <Row key="RegistryExpiryDate" lbl="Registry Expiry Date" val={domain.Registry_Expiry_Date} />}
+      {domain.Registry_Domain_ID && <Row key="RegistryDomainID" lbl="Registry Domain ID" val={domain.Registry_Domain_ID} />}
+      {domain.Registrar_WHOIS_Server && <Row key="RegistrarWhoisServer" lbl="Registrar WHOIS Server" val={domain.Registrar_WHOIS_Server} />}
+      {domain.Registrar && domain.Registrar_URL && <Row key="Registrar" lbl="" val={<a href={domain.Registrar_URL}>{domain.Registrar}</a>} />}
+      {domain.Registrar && !domain.Registrar_URL && <Row key="Registrar" lbl="" val={domain.Registrar} />}
+      {domain.Registrar_IANA_ID && <Row key="RegistrarIANAID" lbl="Registrar IANA ID" val={domain.Registrar_IANA_ID} />}
     </Card>
   );
 }
+
+const cardStyles = `
+  span.val {
+    &.up { color: ${colors.success}; }
+    &.down { color: ${colors.danger}; }
+  }
+`;
 
 export default DomainLookupCard;
