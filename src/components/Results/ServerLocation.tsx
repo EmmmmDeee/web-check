f1e44371d2bec951dcc7ba7cda5c8e07f8a49dc9
EmmmmDeee <158596486+EmmmmDeee@users.noreply.github.com>
2024-05-15 01:56:56 +1000

    improve

--- src/components/Results/ServerLocation.tsx
+++ src/components/Results/ServerLocation.tsx
@@ -1,4 +1,3 @@
-
 import styled from 'styled-components';
 import { ServerLocation } from 'utils/result-processor';
 import { Card } from 'components/Form/Card';
@@ -7,26 +6,37 @@ import Flag from 'components/misc/Flag';
 import { TextSizes } from 'styles/typography';
 import Row, { StyledRow } from 'components/Form/Row';
 
+type CardProps = {
+  children: React.ReactNode;
+  heading: string;
+  actionButtons: any;
+  styles: string;
+};
+
 const cardStyles = '';
 
-const SmallText = styled.span`
+const SmallText = styled.span<{ width: string }>`
   opacity: 0.5;
   font-size: ${TextSizes.xSmall};
   text-align: right;
   display: block;
+  width: ${(props) => props.width};
 `;
 
 const MapRow = styled(StyledRow)`
   padding-top: 1rem;
   flex-direction: column;
+  justify-content: space-between;
+  margin-bottom: 1rem;
 `;
 
 const CountryValue = styled.span`
   display: flex;
   gap: 0.5rem;
+  align-items: center;
 `;
 
-const ServerLocationCard = (props: { data: ServerLocation, title: string, actionButtons: any }): JSX.Element => {
+const ServerLocationCard = (props: { data: ServerLocation; title: string }): JSX.Element => {
   const location = props.data;
   const {
     city, region, country,
@@ -35,24 +45,40 @@ const ServerLocationCard = (props: { data: ServerLocation, title: string, action
   } = location;
 
   return (
-    <Card heading={props.title} actionButtons={props.actionButtons} styles={cardStyles}>
-      <Row lbl="City" val={`${postCode}, ${city}, ${region}`} />
-      <Row lbl="" val="">
-        <b>Country</b>
-        <CountryValue>
-          {country}
-          { countryCode && <Flag countryCode={countryCode} width={28} /> }
-        </CountryValue>
-      </Row>
+    <Card
+      variants={cardStyles}
+      heading={props.title}
+      actionButtons={props.actionButtons}
+    >
+      <Row
+        lbl="City"
+        val={`${postCode}, ${city}, ${region}`}
+      />
+      <Row
+        lbl="Country"
+        val={
+          <CountryValue>
+            {country}
+            {countryCode && <Flag countryCode={countryCode} width={28} title={country} />}
+          </CountryValue>
+        }
+      />
       <Row lbl="Timezone" val={timezone} />
       <Row lbl="Languages" val={languages} />
       <Row lbl="Currency" val={`${currency} (${currencyCode})`} />
       <MapRow>
-        <LocationMap lat={coords.latitude} lon={coords.longitude} label={`Server (${isp})`} />
-        <SmallText>Latitude: {coords.latitude}, Longitude: {coords.longitude} </SmallText>
+        <LocationMap
+          lat={coords.latitude}
+          lon={coords.longitude}
+          label={`Server (${isp})`}
+          for="location-map"
+        />
+        <SmallText width="100%" id="location-map">
+          Latitude: {coords.latitude}, Longitude: {coords.longitude}
+        </SmallText>
       </MapRow>
     </Card>
   );
-}
+};
 
 export default ServerLocationCard;
