912dcad4340418a162130a4d1dcab9e9bef14586
EmmmmDeee <158596486+EmmmmDeee@users.noreply.github.com>
2024-05-15 01:56:39 +1000

    improve

--- src/components/Form/Card.tsx
+++ src/components/Form/Card.tsx
@@ -1,40 +1,42 @@
 import styled from 'styled-components';
-
 import ErrorBoundary from 'components/misc/ErrorBoundary';
 import Heading from 'components/Form/Heading';
 import colors from 'styles/colors';
-import { ReactNode } from 'react';
 
-export const StyledCard = styled.section<{ styles?: string}>`
+export const StyledCard = styled.section<{ styles?: string; height?: string }>`
   background: ${colors.backgroundLighter};
   box-shadow: 4px 4px 0px ${colors.bgShadowColor};
   border-radius: 8px;
   padding: 1rem;
   position: relative;
-  margin 0.5rem;
+  margin: 0.5rem;
   max-height: 64rem;
+  height: ${props => props.height};
   overflow: auto;
+  padding-bottom: 1rem;
   ${props => props.styles}
 `;
 
 interface CardProps {
   children: React.ReactNode;
-  heading?: string,
+  heading?: string;
+  title?: string;
   styles?: string;
-  actionButtons?: ReactNode | undefined;
-};
+  actionButtons?: React.ReactNode | undefined;
+  height?: string;
+}
 
 export const Card = (props: CardProps): JSX.Element => {
-  const { children, heading, styles, actionButtons } = props;
+  const { children, heading, title, styles, actionButtons, height } = props;
   return (
-    <ErrorBoundary title={heading}>
-      <StyledCard styles={styles}>
-        { actionButtons && actionButtons }
-        { heading && <Heading className="inner-heading" as="h3" align="left" color={colors.primary}>{heading}</Heading> }
+    <ErrorBoundary title={title}>
+      <StyledCard styles={styles} height={height}>
+        {actionButtons && actionButtons}
+        {heading && <Heading className="inner-heading" as="h3" align="left" color={colors.primary}>{heading}</Heading>}
         {children}
       </StyledCard>
     </ErrorBoundary>
   );
-}
+};
 
 export default StyledCard;
