fcfee2bb0b639cbef12cf1042f4668b7457d03e5
EmmmmDeee <158596486+EmmmmDeee@users.noreply.github.com>
2024-05-15 01:56:45 +1000

    improve

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,14 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 import PTMono from 'assets/fonts/PTMono.ttf';
 
-export const GlobalStyles = createGlobalStyle`
+const GlobalStyles = createGlobalStyle`
   @font-face {
-    font-family: PTMono;
+    font-family: 'PT Mono';
     font-style: normal;
     font-weight: 400;
-    src: url(${PTMono});
+    src: local('PT Mono'), url(${PTMono}) format('truetype');
   }
-  body { font-family: PTMono; }
+  body {
+    font-family: 'PT Mono', monospace;
+    margin: 0;
+    padding: 0;
+  }
 `;
 
 export default GlobalStyles;
