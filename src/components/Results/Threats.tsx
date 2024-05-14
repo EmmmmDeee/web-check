d24aac67b28962248fc62b3ede9434bd9dea0b93
EmmmmDeee <158596486+EmmmmDeee@users.noreply.github.com>
2024-05-15 01:57:02 +1000

    improve

--- src/components/Results/Threats.tsx
+++ src/components/Results/Threats.tsx
@@ -1,26 +1,31 @@
-
 import styled from 'styled-components';
 import colors from 'styles/colors';
 import { Card } from 'components/Form/Card';
 import Row, { ExpandableRow } from 'components/Form/Row';
 
 const Expandable = styled.details`
-margin-top: 0.5rem;
-cursor: pointer;
-summary::marker {
-  color: ${colors.primary};
-}
+  margin-top: 0.5rem;
+  cursor: pointer;
+  summary::marker {
+    color: ${colors.primary};
+  }
 `;
 
 const getExpandableTitle = (urlObj: any) => {
   let pathName = '';
   try {
     pathName = new URL(urlObj.url).pathname;
-  } catch(e) {}
+  } catch (e) {
+    console.error('Error parsing URL:', e);
+    return 'Invalid URL';
+  }
   return `${pathName} (${urlObj.id})`;
 }
 
-const convertToDate = (dateString: string): string => {
+const convertToDate = (dateString: string | null): string => {
+  if (!dateString) {
+    return '';
+  }
   const [date, time] = dateString.split(' ');
   const [year, month, day] = date.split('-').map(Number);
   const [hour, minute, second] = time.split(':').map(Number);
@@ -31,40 +36,70 @@ const convertToDate = (dateString: string): string => {
   return dateObject.toString();
 }
 
-const MalwareCard = (props: {data: any, title: string, actionButtons: any }): JSX.Element => {
-  const urlHaus = props.data.urlHaus || {};
-  const phishTank = props.data.phishTank || {};
-  const cloudmersive = props.data.cloudmersive || {};
-  const safeBrowsing = props.data.safeBrowsing || {};
+type MalwareCardProps = {
+  data: any;
+  title: string;
+  actionButtons: any;
+}
+
+const MalwareCard = (props: MalwareCardProps): JSX.Element => {
+  const {
+    data: {
+      urlHaus,
+      phishTank,
+      cloudmersive,
+      safeBrowsing,
+    },
+    title,
+    actionButtons,
+  } = props;
+
+  const urlHausData = urlHaus || {};
+  const phishTankData = phishTank || {};
+  const cloudmersiveData = cloudmersive || {};
+  const safeBrowsingData = safeBrowsing || {};
+
   return (
-    <Card heading={props.title} actionButtons={props.actionButtons}>
-      { safeBrowsing && !safeBrowsing.error && (
-        <Row lbl="Google Safe Browsing" val={safeBrowsing.unsafe ? '❌ Unsafe' : '✅ Safe'} />
+    <Card heading={title} actionButtons={actionButtons}>
+      {safeBrowsingData && safeBrowsingData.unsafe !== null && (
+        <Row
+          lbl="Google Safe Browsing"
+          val={safeBrowsingData.unsafe ? '❌ Unsafe' : '✅ Safe'}
+        />
       )}
-      { ((cloudmersive && !cloudmersive.error) || safeBrowsing?.details) && (
-        <Row lbl="Threat Type" val={safeBrowsing?.details?.threatType || cloudmersive.WebsiteThreatType || 'None :)'} />
+      {(cloudmersiveData.error === null || safeBrowsingData.details) && (
+        <Row
+          lbl="Threat Type"
+          val={safeBrowsingData.details?.threatType || cloudmersiveData.WebsiteThreatType || 'None :)'}
+        />
       )}
-      { phishTank && !phishTank.error && (
-        <Row lbl="Phishing Status" val={phishTank?.url0?.in_database !== 'false' ? '❌ Phishing Identified' : '✅ No Phishing Found'} />
+      {phishTankData && phishTankData.error === null && (
+        <Row
+          lbl="Phishing Status"
+          val={phishTankData?.url0?.in_database !== 'false' ? '❌ Phishing Identified' : '✅ No Phishing Found'}
+        />
       )}
-      { phishTank.url0 && phishTank.url0.phish_detail_page && (
-        <Row lbl="" val="">
+      {phishTankData.url0 && phishTankData.url0.phish_detail_page && (
+        <Row
+          lbl=""
+          val=""
+        >
           <span className="lbl">Phish Info</span>
-          <span className="val"><a href={phishTank.url0.phish_detail_page}>{phishTank.url0.phish_id}</a></span>  
+          <span className="val"><a href={phishTankData.url0.phish_detail_page}>{phishTankData.url0.phish_id}</a></span>  
         </Row>
       )}
-      { urlHaus.query_status === 'no_results' && <Row lbl="Malware Status" val="✅ No Malwares Found" />}
-      { urlHaus.query_status === 'ok' && (
+      {urlHausData.query_status === 'no_results' && <Row lbl="Malware Status" val="✅ No Malwares Found" />}
+      {urlHausData.query_status === 'ok' && (
         <>
         <Row lbl="Status" val="❌ Malware Identified" />
-        <Row lbl="First Seen" val={convertToDate(urlHaus.firstseen)} />
-        <Row lbl="Bad URLs Count" val={urlHaus.url_count} />
+        <Row lbl="First Seen" val={convertToDate(urlHausData.firstseen)} />
+        <Row lbl="Bad URLs Count" val={urlHausData.url_count} />
         </>
       )}
-      {urlHaus.urls && (
+      {urlHausData.urls && (
         <Expandable>
           <summary>Expand Results</summary>
-          { urlHaus.urls.map((urlResult: any, index: number) => {
+          { urlHausData.urls.map((urlResult: any, index: number) => {
           const rows = [
             { lbl: 'ID', val: urlResult.id },
             { lbl: 'Status', val: urlResult.url_status },
