b8e06d902c6d85639e39c9dd3d8160f21c9c83b1
EmmmmDeee <158596486+EmmmmDeee@users.noreply.github.com>
2024-05-15 01:56:49 +1000

    improve

--- src/components/misc/Flag.tsx
+++ src/components/misc/Flag.tsx
@@ -1,20 +1,36 @@
+import * as io from 'io-ts';
+import * as is from 'ityped';
+
+const NonEmptyString = io.string.is(s => s.length > 0);
+const CountryAlpha2Code = is.CountryAlpha2Code;
+
 interface Props {
-  countryCode: string,
+  countryCode: CountryAlpha2Code,
   width: number,
 };
 
 const Flag = ({ countryCode, width }: Props): JSX.Element => {
-
   const getFlagUrl = (code: string, w: number = 64) => {
     const protocol = 'https';
     const cdn = 'flagcdn.com';
-    const dimensions = `${width}x${width * 0.75}`;
-    const country = countryCode.toLowerCase();
+    const dimensions = `${w}x${w * 0.75}`;
+    const country = code.toLowerCase();
     const ext = 'png';
     return `${protocol}://${cdn}/${dimensions}/${country}.${ext}`;
   };
 
-  return (<img src={getFlagUrl(countryCode, width)} alt={countryCode} />);
+  const flagUrl = getFlagUrl(countryCode, width);
+
+  return (
+    <img
+      key={flagUrl}
+      src={flagUrl}
+      alt={countryCode}
+    />
+  );
 }
 
 export default Flag;
+
+
+npm install io-ts ityped
