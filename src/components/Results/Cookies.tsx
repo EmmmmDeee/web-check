b6185edca1370f5cab8c036480ee7b5c29d471f4
EmmmmDeee <158596486+EmmmmDeee@users.noreply.github.com>
2024-05-15 01:56:47 +1000

    improve

--- src/components/Results/Cookies.tsx
+++ src/components/Results/Cookies.tsx
@@ -1,42 +1,31 @@
-import { Card } from 'components/Form/Card';
-import { ExpandableRow } from 'components/Form/Row';
+import { Card, ExpandableRow } from 'components/Form';
 import { Cookie } from 'utils/result-processor';
 
-export const parseHeaderCookies = (cookiesHeader: string[]): Cookie[] => {
-  if (!cookiesHeader || !cookiesHeader.length) return [];
-  const cookies = cookiesHeader.flatMap(cookieHeader => {
-    return cookieHeader.split(/,(?=\s[A-Za-z0-9]+=)/).map(cookieString => {
-      const [nameValuePair, ...attributePairs] = cookieString.split('; ').map(part => part.trim());
-      const [name, value] = nameValuePair.split('=');
-      const attributes: Record<string, string> = {};
-      attributePairs.forEach(pair => {
-        const [attributeName, attributeValue = ''] = pair.split('=');
-        attributes[attributeName] = attributeValue;
-      });
-      return { name, value, attributes };
-    });
-  });
-  return cookies;
+type CookieAttributes = Record<string, string>;
+
+const parseCookieString = (cookieString: string): Cookie => {
+  const [nameValuePair, ...attributePairs] = cookieString.split('; ').map(part => part.trim());
+  const [name, value] = nameValuePair.split('=');
+  const attributes = Object.fromEntries(attributePairs.map(pair => pair.split('=')));
+  return { name, value, attributes };
 };
 
-const CookiesCard = (props: { data: any, title: string, actionButtons: any}): JSX.Element => {
-  const headerCookies = parseHeaderCookies(props.data.headerCookies) || [];
-  const clientCookies = props.data.clientCookies || [];
+const CookiesCard = (props: { data: { headerCookies?: string[], clientCookies?: Cookie[] }, title: string, actionButtons: JSX.Element }): JSX.Element => {
+  const headerCookies = (props.data.headerCookies || []).map(parseCookieString);
+  const clientCookies = (props.data.clientCookies || []).map(cookie => ({ ...cookie, attributes: Object.fromEntries(Object.entries(cookie.attributes).map(([key, value]) => [key, value.toString()])) }));
   return (
     <Card heading={props.title} actionButtons={props.actionButtons}>
       {
-        headerCookies.map((cookie: any, index: number) => {
-          const attributes = Object.keys(cookie.attributes).map((key: string) => {
-            return { lbl: key, val: cookie.attributes[key] }
-          });
+        headerCookies.map((cookie: Cookie, index: number) => {
+          const attributes = Object.keys(cookie.attributes).map((key: string) => ({ lbl: key, val: cookie.attributes[key] }));
           return (
             <ExpandableRow key={`cookie-${index}`} lbl={cookie.name} val={cookie.value} rowList={attributes} />
-          )
+          );
         })
       }
       {
-        clientCookies.map((cookie: any) => {
-          const nameValPairs = Object.keys(cookie).map((key: string) => { return { lbl: key, val: cookie[key] }});
+        clientCookies.map((cookie: Cookie) => {
+          const nameValPairs = Object.keys(cookie).map((key: string) => ({ lbl: key, val: cookie[key] }));
           return (
             <ExpandableRow key={`cookie-${cookie.name}`} lbl={cookie.name} val="" rowList={nameValPairs} />
           );
