282ecf3b9d5055d54cd510b8c9f5158508ba1026
EmmmmDeee <158596486+EmmmmDeee@users.noreply.github.com>
2024-05-15 01:56:42 +1000

    improve

--- src/components/Results/TraceRoute.tsx
+++ src/components/Results/TraceRoute.tsx
@@ -1,64 +1,79 @@
 import styled from 'styled-components';
 import colors from 'styles/colors';
-import { Card } from 'components/Form/Card';
+import { Card, CardProps } from 'components/Form/Card';
 
-const RouteRow = styled.div`
+type RouteRowProps = {
+  ipName: string;
+};
+
+const RouteRow = styled.div<RouteRowProps>`
   text-align: center;
   width: fit-content;
   margin: 0 auto;
+
   .ipName {
     font-size: 1rem;
   }
 `;
 
-const RouteTimings = styled.div`
-p {
-  margin: 0 auto;
-}
-.arrow {
-  font-size: 2.5rem;
-  color: ${colors.primary};
-  margin-top: -1rem;
-}
-.times {
-  font-size: 0.85rem;
-  color: ${colors.textColorSecondary};
-}
-.completed {
-  text-align: center;
-  font-weight: bold;
-}
+type RouteTimingsProps = {
+  timings: number[];
+};
+
+const RouteTimings = styled.div<RouteTimingsProps>`
+  p {
+    margin: 0 auto;
+    line-height: 1.5;
+  }
+
+  .arrow {
+    font-size: 2.5rem;
+    color: ${colors.primary};
+    margin-top: -1rem;
+    display: inline-block;
+    vertical-align: middle;
+  }
+
+  .times {
+    font-size: 0.85rem;
+    color: ${colors.textColorSecondary};
+    display: inline-block;
+    vertical-align: middle;
+  }
+
+  .completed {
+    text-align: center;
+    font-weight: bold;
+  }
 `;
 
-const cardStyles = ``;
+const TraceRouteCard: React.FC<CardProps & { data: { result: { [ip: string]: number[] }[]; timeTaken: number } }> = (props) => {
+  const { data: { result: routes, timeTaken }, title, actionButtons } = props;
 
-const TraceRouteCard = (props: { data: any, title: string, actionButtons: any }): JSX.Element => {
-  const traceRouteResponse = props.data;
-  const routes = traceRouteResponse.result;
   return (
-    <Card heading={props.title} actionButtons={props.actionButtons} styles={cardStyles}>
-      {routes.filter((x: any) => x).map((route: any, index: number) => (
-          <RouteRow key={index}>
-            <span className="ipName">{Object.keys(route)[0]}</span>
-            <RouteTimings>
-              {route[Object.keys(route)[0]].map((time: any, packetIndex: number) => (
-                <p className="times" key={`timing-${packetIndex}-${time}`}>
-                  { route[Object.keys(route)[0]].length > 1 && (<>Packet #{packetIndex + 1}:</>) }
-                  Took {time} ms
-                </p>
-              ))}
-              <p className="arrow">↓</p>
-            </RouteTimings>
-          </RouteRow>
-        )
-      )}
-      <RouteTimings>
-        <p className="completed">
-          Round trip completed in {traceRouteResponse.timeTaken} ms
+    <Card heading={title} actionButtons={actionButtons} styles={cardStyles}>
+      {routes.map((route, index) => (
+        <RouteRow key={index} ipName={Object.keys(route)[0]}>
+          <RouteTimings timings={route[Object.keys(route)[0]]}>
+            {route[Object.keys(route)[0]].map((time, packetIndex) => (
+              <p className="times" key={`timing-${packetIndex}-${time}`}>
+                {route[Object.keys(route)[0]].length > 1 && <>Packet #{packetIndex + 1}: </>}
+                Took {time} ms
+              </p>
+            ))}
+            <p className="arrow" role="img" aria-label="Down arrow">↓</p>
+          </RouteTimings>
+        </RouteRow>
+      ))}
+      <RouteTimings timings={[]} className="completed">
+        <p>
+          Round trip completed in {timeTaken} ms
         </p>
       </RouteTimings>
     </Card>
   );
 }
+
+const cardStyles = '';
 
 export default TraceRouteCard;
