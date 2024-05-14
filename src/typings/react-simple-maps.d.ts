00910253fa2555b5f3a274f45032393548d33622
EmmmmDeee <158596486+EmmmmDeee@users.noreply.github.com>
2024-05-15 01:56:46 +1000

    improve

--- src/typings/react-simple-maps.d.ts
+++ src/typings/react-simple-maps.d.ts
@@ -1,1 +1,29 @@
-declare module 'react-simple-maps';
+// src/index.js
+
+import React from 'react';
+import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
+
+const geoUrl =
+  'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-files/world-110m.json';
+
+const MapChart = () => {
+  return (
+    <ComposableMap projectionConfig={{ scale: 200 }}>
+      <Geographies geography={geoUrl}>
+        {({ geographies }) =>
+          geographies.map((geo) => (
+            <Geography
+              key={geo.rsmKey}
+              geography={geo}
+              fill="#EAEAEC"
+              stroke="#D8D8D8"
+              strokeWidth={0.5}
+            />
+          ))
+        }
+      </Geographies>
+    </ComposableMap>
+  );
+};
+
+export default MapChart;
