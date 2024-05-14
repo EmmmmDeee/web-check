f20d1ad0d676ee7df85fc9a278bfb8d635bbd97d
EmmmmDeee <158596486+EmmmmDeee@users.noreply.github.com>
2024-05-15 01:56:52 +1000

    improve

--- src/components/Results/ServerInfo.tsx
+++ src/components/Results/ServerInfo.tsx
@@ -2,21 +2,28 @@ import { ServerInfo } from 'utils/result-processor';
 import { Card } from 'components/Form/Card';
 import Row from 'components/Form/Row';
 
-const ServerInfoCard = (props: { data: ServerInfo, title: string, actionButtons: any }): JSX.Element => {
+type ServerInfoCardProps = {
+  data: ServerInfo;
+  title: string;
+  actionButtons: any;
+};
+
+const ServerInfoCard = (props: ServerInfoCardProps): JSX.Element => {
   const info = props.data;
-  const { org, asn, isp, os, ports, ip, loc, type } = info;
   return (
     <Card heading={props.title} actionButtons={props.actionButtons}>
-      { org && <Row lbl="Organization" val={org} /> }
-      { (isp && isp !== org) && <Row lbl="Service Provider" val={isp} /> }
-      { os && <Row lbl="Operating System" val={os} /> }
-      { asn && <Row lbl="ASN Code" val={asn} /> }
-      { ports && <Row lbl="Ports" val={ports} /> }
-      { ip && <Row lbl="IP" val={ip} /> }
-      { type && <Row lbl="Type" val={type} /> }
-      { loc && <Row lbl="Location" val={loc} /> }
+      {info.org && <Row key="org" lbl="Organization" val={info.org ?? ''} />}
+      {info.isp && info.isp !== info.org && (
+        <Row key="isp" lbl="Service Provider" val={info.isp ?? ''} />
+      )}
+      {info.os && <Row key="os" lbl="Operating System" val={info.os ?? ''} />}
+      {info.asn && <Row key="asn" lbl="ASN Code" val={info.asn ?? ''} />}
+      {info.ports && <Row key="ports" lbl="Ports" val={info.ports ?? ''} />}
+      {info.ip && <Row key="ip" lbl="IP" val={info.ip ?? ''} />}
+      {info.type && <Row key="type" lbl="Type" val={info.type ?? ''} />}
+      {info.loc && <Row key="loc" lbl="Location" val={info.loc ?? ''} />}
     </Card>
   );
-}
+};
 
 export default ServerInfoCard;
