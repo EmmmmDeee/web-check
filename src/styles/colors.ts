8c9704971e56c02a17815d561c283e46d5b2704f
EmmmmDeee <158596486+EmmmmDeee@users.noreply.github.com>
2024-05-15 01:56:52 +1000

    improve

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,5 +1,31 @@
+// Define a type for the color object properties
+type ColorObjectProperty = {
+  readonly [key: string]: string;
+};
 
-const colors = {
+// Define a type for the color object, specifying the property keys and values
+type ColorObject = ColorObjectProperty & {
+  primary: string;
+  primaryLighter: string;
+  textColor: string;
+  textColorSecondary: string;
+  background: string;
+  backgroundDarker: string;
+  backgroundLighter: string;
+  bgShadowColor: string;
+  fgShadowColor: string;
+  primaryTransparent: string;
+  // Action Colors
+  info: string;
+  success: string;
+  warning: string;
+  error: string;
+  danger: string;
+  neutral: string;
+};
+
+// Initialize the color object with the specified properties
+const colors: ColorObject = {
   primary: '#9fef00',
   primaryLighter: '#cff97a',
   textColor: '#ffffff',
@@ -10,7 +36,6 @@ const colors = {
   bgShadowColor: '#0f1620',
   fgShadowColor: '#456602',
   primaryTransparent: '#9fef0012',
-
   // Action Colors
   info: '#04e4f4',
   success: '#20e253',
@@ -20,4 +45,5 @@ const colors = {
   neutral: '#272f4d',
 };
 
+// Export the color object as the default export
 export default colors;
