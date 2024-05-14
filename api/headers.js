211e4bc3dc6ac05a1caf39f0d2179a9bbe880744
EmmmmDeee <158596486+EmmmmDeee@users.noreply.github.com>
2024-05-15 01:56:23 +1000

    improve

--- api/headers.js
+++ api/headers.js
@@ -1,19 +1,26 @@
 const axios = require('axios');
 const middleware = require('./_common/middleware');
 
+// Handler function to make GET request to the given URL
 const handler = async (url, event, context) => {
   try {
-    const response = await axios.get(url, {
-      validateStatus: function (status) {
-        return status >= 200 && status < 600; // Resolve only if the status code is less than 600
-      },
-    });
+    const response = await axios.get(url);
 
-    return response.headers;
+    // Check if the status code is in the range of 200-599
+    if (response.status >= 200 && response.status < 600) {
+      return response.headers;
+    } else {
+      throw new Error(`Received status code ${response.status} from ${url}`);
+    }
   } catch (error) {
-    throw new Error(error.message);
+    // If the error is related to the status code, throw a new error
+    if (error.response && error.response.status) {
+      throw new Error(error.response.statusText);
+    } else {
+      throw new Error(error.message);
+    }
   }
 };
 
+// Export the middleware-wrapped handler function
 module.exports = middleware(handler);
-module.exports.handler = middleware(handler);
