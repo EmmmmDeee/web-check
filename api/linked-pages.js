989264293f8fc83db417cac102ca90f9acd63649
EmmmmDeee <158596486+EmmmmDeee@users.noreply.github.com>
2024-05-15 01:56:30 +1000

    improve

--- api/linked-pages.js
+++ api/linked-pages.js
@@ -3,47 +3,59 @@ const cheerio = require('cheerio');
 const urlLib = require('url');
 const middleware = require('./_common/middleware');
 
-const handler = async (url) => {
-  const response = await axios.get(url);
-  const html = response.data;
-  const $ = cheerio.load(html);
-  const internalLinksMap = new Map();
-  const externalLinksMap = new Map();
+const linkHandler = async (url) => {
+  try {
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const linkMaps = {
+      internal: new Map(),
+      external: new Map(),
+    };
 
-  // Get all links on the page
-  $('a[href]').each((i, link) => {
-    const href = $(link).attr('href');
-    const absoluteUrl = urlLib.resolve(url, href);
-    
-    // Check if absolute / relative, append to appropriate map or increment occurrence count
-    if (absoluteUrl.startsWith(url)) {
-      const count = internalLinksMap.get(absoluteUrl) || 0;
-      internalLinksMap.set(absoluteUrl, count + 1);
-    } else if (href.startsWith('http://') || href.startsWith('https://')) {
-      const count = externalLinksMap.get(absoluteUrl) || 0;
-      externalLinksMap.set(absoluteUrl, count + 1);
-    }
-  });
+    // Get all links on the page
+    $('a[href]').each((i, link) => {
+      const href = $(link).attr('href');
+      const absoluteUrl = urlLib.resolve(url, href);
 
-  // Sort by most occurrences, remove supplicates, and convert to array
-  const internalLinks = [...internalLinksMap.entries()].sort((a, b) => b[1] - a[1]).map(entry => entry[0]);
-  const externalLinks = [...externalLinksMap.entries()].sort((a, b) => b[1] - a[1]).map(entry => entry[0]);
+      // Check if absolute / relative, append to appropriate map or increment occurrence count
+      const linkType = absoluteUrl.startsWith(url) ? 'internal' : 'external';
+      const count = linkMaps[linkType].get(absoluteUrl) || 0;
+      linkMaps[linkType].set(absoluteUrl, count + 1);
+    });
 
-  // If there were no links, then mark as skipped and show reasons
-  if (internalLinks.length === 0 && externalLinks.length === 0) {
+    // Sort by most occurrences, remove supplicates, and convert to array
+    const linkArrays = Object.entries(linkMaps).map(([key, map]) => {
+      return [...map.entries()]
+        .sort((a, b) => b[1] - a[1])
+        .map(entry => entry[0]);
+    });
+
     return {
-      statusCode: 400,
-      body: JSON.stringify({
-        skipped: 'No internal or external links found. '
-          + 'This may be due to the website being dynamically rendered, using a client-side framework (like React), and without SSR enabled. '
-          + 'That would mean that the static HTML returned from the HTTP request doesn\'t contain any meaningful content for Web-Check to analyze. '
-          + 'You can rectify this by using a headless browser to render the page instead.',
+      [linkArrays[0][0]]: linkArrays[0][1],
+      [linkArrays[1][0]]: linkArrays[1][1],
+    };
+
+  } catch (error) {
+    if (!error.response) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          error: 'No response received from the server. Please check the URL and try again.',
         }),
+      };
+    }
+
+    const { status, statusText, data } = error.response;
+    return {
+      statusCode: status,
+      body: JSON.stringify({
+        error: `Error ${status}: ${statusText}\n${data}`,
+      }),
     };
   }
-
-  return { internal: internalLinks, external: externalLinks };
 };
 
-module.exports = middleware(handler);
-module.exports.handler = middleware(handler);
+const handler = middleware(linkHandler);
+module.exports = { handler, linkHandler };
+
