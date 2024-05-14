74a3d2ad1e59519d4104fc2c325c68a9803ef1fd
EmmmmDeee <158596486+EmmmmDeee@users.noreply.github.com>
2024-05-15 01:56:25 +1000

    improve

--- api/rank.js
+++ api/rank.js
@@ -1,26 +1,45 @@
 const axios = require('axios');
 const middleware = require('./_common/middleware');
 
-const handler = async (url) => { 
+const handler = async (url) => {
   const domain = url ? new URL(url).hostname : null;
   if (!domain) throw new Error('Invalid URL');
 
+  const auth = (process.env.TRANCO_API_KEY && {
+    auth: {
+      username: process.env.TRANCO_USERNAME,
+      password: process.env.TRANCO_API_KEY,
+    },
+  }) || {};
+
+  const requestOptions = {
+    method: 'GET',
+    baseURL: 'https://tranco-list.eu/api/ranks/domain/',
+    url: domain,
+    timeout: 5000,
+    ...auth,
+  };
+
   try {
-    const auth = process.env.TRANCO_API_KEY ? // Auth is optional.
-      { auth: { username: process.env.TRANCO_USERNAME, password: process.env.TRANCO_API_KEY } }
-      : {};
-    const response = await axios.get(
-      `https://tranco-list.eu/api/ranks/domain/${domain}`, { timeout: 5000 }, auth,
-      );
+    const response = await axios.request(requestOptions);
+
     if (!response.data || !response.data.ranks || response.data.ranks.length === 0) {
-      return { skipped: `Skipping, as ${domain} isn't ranked in the top 100 million sites yet.`};
+      return { skipped: `Skipping, as ${domain} isn't ranked in the top 100 million sites yet.` };
     }
+
+    if (response.status !== 200) {
+      throw new Error(`Unexpected status code: ${response.status}`);
+    }
+
     return response.data;
   } catch (error) {
+    if (error.response) {
+      return { error: `Unable to fetch rank, HTTP response error: ${error.response.status} - ${error.response.statusText}` };
+    }
     return { error: `Unable to fetch rank, ${error.message}` };
   }
 };
 
-module.exports = middleware(handler);
-module.exports.handler = middleware(handler);
+module.exports = handler;
+module.exports.middleware = middleware(handler);
 
